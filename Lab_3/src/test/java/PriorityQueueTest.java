bca83bdde25620901c76f56a8c7637b97a0824ff
xizhen <xizhen@cs.nctu.edu.tw>
2022-03-13 15:33:21 +0800

    [fix] pom.xml

--- Lab_3/src/test/java/PriorityQueueTest.java
+++ Lab_3/src/test/java/PriorityQueueTest.java
@@ -1,4 +1,3 @@
-import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
