a3782337a4daf2320a068b888c34f61efe71f2e3
xizhen <xizhen@cs.nctu.edu.tw>
2022-05-27 00:34:18 +0800

    add: test cases *10

--- Homework/HW4/CalTest.java
+++ Homework/HW4/CalTest.java
@@ -4,11 +4,59 @@ import org.junit.Test;
 public class CalTest {
     @Test
     public void test1() {
-        Assert.assertEquals(365, Cal.cal(1, 1, 12, 31, 2012));
+        Assert.assertEquals(364, Cal.cal(1, 1, 12, 31, 2021));
     }
-    
+
     @Test
     public void test2() {
-        Assert.assertEquals(90, Cal.cal(1, 1, 3, 31, 396));
+        // leap year
+        Assert.assertEquals(365, Cal.cal(1, 1, 12, 31, 2020));
+    }
+
+    @Test
+    public void test3() {
+        // same month
+        Assert.assertEquals(30, Cal.cal(1, 1, 1, 31, 2020));
+    }
+
+    @Test
+    public void test4() {
+        // zero day
+        Assert.assertEquals(0, Cal.cal(1, 1, 1, 1, 2020));
+    }
+
+    @Test
+    public void test5() {
+        // month1 > month2
+        Assert.assertEquals(29, Cal.cal(8, 8, 7, 7, 2020));
+    }
+
+    @Test
+    public void test6() {
+        Assert.assertEquals(89, Cal.cal(1, 1, 3, 31, 1700));
+    }
+
+    @Test
+    public void test7() {
+        // (m4 == 0) (m100 == 0) (m400 == 0)
+        Assert.assertEquals(90, Cal.cal(2, 1, 5, 1, 1600));
+    }
+
+    @Test
+    public void test8() {
+        // (m4 < 0) (m100 < 0) (m400 < 0)
+        Assert.assertEquals(89, Cal.cal(2, 1, 5, 1, -1));
+    }
+
+    @Test
+    public void test9() {
+        // (m4 == 0) (m100 < 0) (m400 < 0)
+        Assert.assertEquals(89, Cal.cal(2, 1, 5, 1, -4));
+    }
+
+    @Test
+    public void test10() {
+        // (m4 == 0) (m100 == 0) (m400 < 0)
+        Assert.assertEquals(89, Cal.cal(2, 1, 5, 1, -500));
     }
 }
